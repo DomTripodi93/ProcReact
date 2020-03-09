dbbf4f41f032b05b9206af638513810f0e1eca4d
DomTripodi93 <dtripodi93@gmail.com>
2020-03-09 11:30:22 -0400

    added basic welcome page and core stylesheet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
 import Header from './shared/header/header';
+import Home from './containers/home/home';
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Switch>
+        <Route exact path='/' component={Home} />
+      </Switch>
     </div>
   );
 };
---
+++ frontend/src/containers/home/home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => { 
+    return (
+        <div className="border centered">
+            <div>
+                <h2 className="spaced">Welcome to Process Manager V 0.1.0</h2>
+                <h4 className="spaced">
+                    To get started <Link to="/register">sign up</Link> for a new account
+                </h4>
+                <h4 className="spaced">
+                    If you already have an account, you can <Link to='/login'>log in</Link>
+                </h4>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
