aaa9a167f2d68e73dc2869d4cfa45e9da3796e38
DomTripodi93 <dtripodi93@gmail.com>
2020-03-13 00:30:24 -0400

    added dynamic header navigation options

--- frontend/src/shared/header/header.js
+++ frontend/src/shared/header/header.js
@@ -1,40 +1,62 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import logo from '../../assets/favicon.png';
 import './header.styles.scss';
 
-const Header = () => {
+const Header = props => {
+    const [authValue, setAuthValue] = useState(props.isAuthenticated);
+  
+    useEffect(() => {
+      setAuthValue(props.isAuthenticated)
+    }, [props]);
+
     return (
-        <div className='header'>
-            <Link to='/' className='logo-holder'>
-                <img className='logo-holder' alt='logo' src={logo}></img>
-            </Link>
-            <div className='routes'>
-                <Link to='/' className='route'>
-                    Home
-                </Link>
-                <Link to='/schedule' className='route'>
-                    Schedule
-                    <span className='arrow'></span>
-                </Link>
-                <Link to='/departments' className='route'>
-                    Departments
-                </Link>
-            </div>
-            <div className='edge'>
-                <Link to='/signout' className='route'>
-                    Log Out
-                </Link>            
-            </div>
+        <div>
+            {authValue ?
+                <div className='header'>
+                    <Link to='/' className='logo-holder'>
+                        <img className='logo-holder' alt='logo' src={logo}></img>
+                    </Link>
+                    <div className='routes'>
+                        <Link to='/' className='route'>
+                            Home
+                        </Link>
+                        <Link to='/schedule' className='route'>
+                            Schedule &#x21af;
+                        </Link>
+                        <Link to='/departments' className='route'>
+                            Departments
+                        </Link>
+                    </div>
+                    <div className='edge'>
+                        <Link to='/signout' className='route'>
+                            Log Out
+                        </Link>            
+                    </div>
+                </div>
+                :
+                <div className='header'>
+                    <Link to='/' className='logo-holder'>
+                        <img className='logo-holder' alt='logo' src={logo}></img>
+                    </Link>
+                    <div className='routes'>
+                        <Link to='/signup' className='route'>
+                            Sign Up
+                        </Link>
+                        <Link to='/signin' className='route'>
+                            Sign In
+                        </Link>
+                    </div>
+                    <div className='edge'></div>
+                </div>
+            }
         </div>
     )
 }
 
-
 const mapStateToProps = state => ({
     isAuthenticated: state.user.isAuthenticated
-  });
-
+});
 
 export default connect(mapStateToProps)(Header);
