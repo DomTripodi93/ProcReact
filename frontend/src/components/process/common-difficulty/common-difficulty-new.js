e1fd43c78c7307168f1143316720f105d3ed98b5
DomTripodi93 <dtripodi93@gmail.com>
2020-03-18 17:38:16 -0400

    added common difficulty new component functionality

--- frontend/src/components/process/common-difficulty/common-difficulty-new.js
+++ frontend/src/components/process/common-difficulty/common-difficulty-new.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import CustomButton from '../../../shared/elements/button/custom-button.component';
+import CommonDifficultyForm from './common-difficulty-form';
 
-const CommonDifficultyNew = props => {
+const CommonDifficultyNew = (props) =>{
     return(
         <div>
-            common difficulty new
+            {props.addMode ?
+                <div>
+                    <div className='border'>
+                        <CommonDifficultyForm 
+                            deptName={props.deptName} 
+                            objectiveName={props.objectiveName}
+                            callback={props.action} 
+                            editMode={false}/>
+                    </div>
+                    <br />
+                </div>
+            :
+                <div className='full-button'>
+                    <div className='grid100'>
+                        <CustomButton 
+                            buttonStyle="blue"
+                            label="Add Common Difficulty"
+                            action={props.action} 
+                            />
+                    </div>
+                </div>
+            }
         </div>
     )
 }
