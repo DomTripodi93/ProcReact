f07c9d1fdcb38aa0a73a6ca166397a0210abad9e
DomTripodi93 <dtripodi93@gmail.com>
2020-03-17 23:09:03 -0400

    set up step-new step header

--- frontend/src/components/process/step/step-new.js
+++ frontend/src/components/process/step/step-new.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import CustomButton from '../../../shared/elements/button/custom-button.component';
+import StepForm from './step-form';
 
-const StepNew = props => {
+const StepNew = (props) =>{
     return(
         <div>
-            step new
+            {props.addMode ?
+                <div>
+                    <div className='border'>
+                        <StepForm 
+                            deptName={props.deptName} 
+                            callback={props.action} 
+                            editMode={false}/>
+                    </div>
+                    <br />
+                </div>
+            :
+                <div className='full-button'>
+                    <div className='grid100'>
+                        <CustomButton 
+                            buttonStyle="blue"
+                            label="Add Step"
+                            action={props.action} 
+                            />
+                    </div>
+                </div>
+            }
         </div>
     )
 }
