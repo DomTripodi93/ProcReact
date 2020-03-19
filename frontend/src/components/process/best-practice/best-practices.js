f93f02060538bd4368171b47add57d4f2fef50b9
DomTripodi93 <dtripodi93@gmail.com>
2020-03-18 22:21:07 -0400

    set up best practices display container

--- frontend/src/components/process/best-practice/best-practices.js
+++ frontend/src/components/process/best-practice/best-practices.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import SingleBestPractice from './single-best-practice';
 
 const BestPractices = props => {
     return(
         <div>
-            best pratices
+            {props.bestPractices.length > 0 ?
+                <div>
+                    <div className='grid50'>
+                        {props.bestPractices.map(bestPractice=>(
+                            <div 
+                                key={bestPractice.id}
+                                >
+                                <SingleBestPractice
+                                    bestPractice={bestPractice}
+                                    inDept={true} />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                :
+                <div className="border centered">
+                    <h4 className="spaced">
+                        You currently don't have any common difficulties! 
+                    </h4>
+                    <h4 className="spaced">
+                        Add some common difficulties using the button above to see them here.
+                    </h4>
+                </div>
+            }
         </div>
     )
 }
+
 
 export default BestPractices;
