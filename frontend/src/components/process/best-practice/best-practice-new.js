258c952e9428338d3186789e322757292a344a88
DomTripodi93 <dtripodi93@gmail.com>
2020-03-18 22:22:47 -0400

    set up best practice new component

--- frontend/src/components/process/best-practice/best-practice-new.js
+++ frontend/src/components/process/best-practice/best-practice-new.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import CustomButton from '../../../shared/elements/button/custom-button.component';
+import BestPracticeForm from './best-practice-form';
 
-const BestPracticeNew = props => {
+const BestPracticeNew = (props) =>{
     return(
         <div>
-            best practice new
+            {props.addMode ?
+                <div>
+                    <div className='border'>
+                        <BestPracticeForm 
+                            deptName={props.deptName} 
+                            objectiveName={props.objectiveName}
+                            stepNumber={props.stepNumber}
+                            callback={props.action} 
+                            editMode={false}/>
+                    </div>
+                    <br />
+                </div>
+            :
+                <div className='full-button'>
+                    <div className='grid100'>
+                        <CustomButton 
+                            buttonStyle="blue"
+                            label="Add Common Difficulty"
+                            action={props.action} 
+                            />
+                    </div>
+                </div>
+            }
         </div>
     )
 }
