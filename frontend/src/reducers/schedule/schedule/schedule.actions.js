76b17f939c6bb9c16e8de5a90e66d16a09a43283
DomTripodi93 <dtripodi93@gmail.com>
2020-04-05 00:18:42 -0400

    updated reducer methodology and actions for scheduled tasks

--- frontend/src/reducers/schedule/schedule/schedule.actions.js
+++ frontend/src/reducers/schedule/schedule/schedule.actions.js
@@ -8,7 +8,7 @@ export function fetchSchedulesByDate(month, day, year){
     return dispatch => {
         http.fetchAll("schedule/byUser/" + month + "&" + year + "&" + day)
             .then((schedules) => {
-                dispatch(setSchedules(schedules, "all"));
+                dispatch(setSchedules(schedules, year + "/" + month + "/" + day));
             });
     }
 }
@@ -18,73 +18,76 @@ export function fetchSchedulesByEmployee(employeeId, month, day, year){
     return dispatch => {
         http.fetchAll("schedule/byEmployee/" + employeeId + "&" + month + "&" + year + "&" + day)
             .then((schedules) => {
-                dispatch(setSchedules(schedules, employeeId));
+                dispatch(setSchedules(schedules, employeeId + "/" + year + "/" + month + "/" + day));
             });
     }
 }
 //Gets all schedules for a specific day
 
-export function addScheduledTask(schedule, callback){
+export function addScheduledTask(schedule, callback, date){
     return dispatch =>{
         http.addItem("schedule", schedule)
             .then(addedSchedule =>{
-                dispatch(addScheduleToState(addedSchedule.data));
+                dispatch(addScheduleToState(addedSchedule.data, date));
                 callback();
             });
     }
 }
 //Posts new schedule to API
 
-export function updateScheduledTask(schedule, callback){
+export function updateScheduledTask(schedule, callback, date){
     return dispatch =>{
         http.updateItemById("schedule", schedule, schedule.id)
             .then(() =>{
-                dispatch(updateSchedulesInState(schedule));
+                dispatch(updateSchedulesInState(schedule, date));
                 callback();
             });
     }
 }
 //Updates schedule in database
 
-export function deleteSchedule(id){
+export function deleteSchedule(id, date){
     return dispatch =>{
         http.deleteItemById("schedule", id)
             .then(()=>{
-                dispatch(deleteScheduleFromState(id));
+                dispatch(deleteScheduleFromState(id, date));
             });
     }
 }
 //Deletes selected schedule
 
-export function addScheduleToState(schedule){
+export function addScheduleToState(schedule, date){
     return {
         type: ScheduleActionTypes.ADD_SCHEDULE,
-        payload: schedule
+        payload: schedule,
+        date
     }
 }
 //Adds new schedule from post to state
 
-export function setSchedules(schedules, setFor){
+export function setSchedules(schedules, date){
     return {
         type: ScheduleActionTypes.SET_SCHEDULES,
         payload: schedules,
-        setFor
+        date
     }
 }
 //Sets all schedules in state
 
-export function updateSchedulesInState(schedule){
+export function updateSchedulesInState(schedule, date){
     return {
         type: ScheduleActionTypes.UPDATE_SCHEDULES,
-        payload: schedule
+        payload: schedule,
+        date
     }
 }
 //Updates function for schedule
 
-export function deleteScheduleFromState(id){
+export function deleteScheduleFromState(id, date){
     return {
         type: ScheduleActionTypes.DELETE_SCHEDULE,
-        payload: id
+        payload: id,
+        date
     }
 }
 //Deletes selected schedule
--- frontend/src/reducers/schedule/schedule/schedule.reducer.js
+++ frontend/src/reducers/schedule/schedule/schedule.reducer.js
@@ -1,58 +1,68 @@
 import ScheduleActionTypes from './schedule.types';
 
 const INITIAL_STATE = {
-    scheduledTasks: {}
+    scheduledTasks: {},
+    selectedScheduledTasks: []
 }
 
 const scheduleReducer = (state = INITIAL_STATE, action) => {
     let taskHold = state.scheduledTasks;
+    let selectedHold = state.selectedScheduledTasks;
+    const filterTasks = (taskArray, id) =>{
+        return taskArray.filter((value)=>{
+            return value.id !== id 
+        })
+    }
+    const sortTasks = (taskArray) =>{
+        return taskArray.sort((first, second)=>{ 
+            if (first.date > second.date){
+                return 1;
+            } else {
+                return -1;
+            }
+        });
+    }
     switch (action.type) {
         case ScheduleActionTypes.SET_SCHEDULES:
-            taskHold[action.date] = action.payload.data; 
+            taskHold[action.date] = action.payload.data;
+            selectedHold = taskHold[action.date];
+            return {
+                scheduledTasks: taskHold,
+                selectedScheduledTasks: selectedHold
+            };
+        case ScheduleActionTypes.SELECT_SCHEDULES:
+            selectedHold = action.payload;
             return {
                 ...state,
-                scheduledTasks: action.payload.data
+                selectedScheduledTasks: selectedHold
             };
         case ScheduleActionTypes.ADD_SCHEDULE:
-            taskHold[action.date].push(action.payload)
-            taskHold[action.date] = taskHold[action.date]
-                .sort((first, second)=>{ 
-                    if (first.date > second.date){
-                        return 1;
-                    } else {
-                        return -1;
-                    }
-                });
+            taskHold[action.date].push(action.payload);
+            taskHold[action.date] = sortTasks(taskHold[action.date])
+            selectedHold = taskHold[action.date]
             return {
-                scheduledTasks: taskHold
+                scheduledTasks: taskHold,
+                selectedScheduledTasks: selectedHold
             };
         case ScheduleActionTypes.UPDATE_SCHEDULES:
             taskHold[action.date].push(action.payload)
-            taskHold[action.date] = taskHold[action.date]
-                .filter((value)=>{
-                    return value.id !== action.payload.id 
-                })
-                .sort((first, second)=>{
-                    if (first.date > second.date){
-                        return 1
-                    } else {
-                        return -1
-                    }}
-                )
+            taskHold[action.date] = sortTasks(filterTasks(taskHold[action.date], action.payload.id));
+            selectedHold = taskHold[action.date]
             return {
-                scheduledTasks: taskHold
+                scheduledTasks: taskHold,
+                selectedScheduledTasks: selectedHold
             };
         case ScheduleActionTypes.DELETE_SCHEDULE:
-            taskHold[action.date] = taskHold[action.date]
-                .filter((value)=>{
-                    return value.id !== action.payload
-                })
+            taskHold[action.date] = filterTasks(taskHold[action.date], action.payload.id);
+            selectedHold = taskHold[action.date]
             return {
-                scheduledTasks: taskHold
+                scheduledTasks: taskHold,
+                selectedScheduledTasks: selectedHold
             };
         case ScheduleActionTypes.SIGNOUT_USER:
             return {
-                scheduledTasks: {}
+                scheduledTasks: {},
+                selectedScheduledTasks: []
             }
         default:
             return state;
